dbb3eb53302b52aecc292e8a2b20e1a4b9c38e41
mdsanaulhaque <mdsanaulhaqu81@gmail.com>
2021-02-08 23:47:58 +0600

    kam hoche

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,11 +1,15 @@
 const searchMeals = () => {
     const searchText = document.getElementById('search-field').value;
-    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+   if(searchText ===""){   
+    }
+    else{
+  const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals))
     
-}
+        }
+    }
 const displayMeals = meals => {
     const mealContainer = document.getElementById('meal-container')
     mealContainer.innerHTML = '';
